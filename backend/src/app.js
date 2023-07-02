58665951ca237bf22f0368ee4bceb4b02a1efd0a
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2023-07-02 11:21:01 -0300

    Adicionado o Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
 // Nossa API saberá usar JSON
 app.use(express.json());
+
+// Liberando Cors para qualquer client se conectar a nossa API.
+app.use(cors());
 
 // toda requisição será direcionada para o arquivo router.
 app.use(router);
